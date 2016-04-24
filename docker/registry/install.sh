84d6e57c644f00f1408b7c90df91296a31167f62
welion <353566165@qq.com>
2016-04-24 12:14:11 +0800

    添加一个失败的registry安装
    添加一个失败的registry安装

---
+++ docker/registry/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get install -y build-essential python-dev libevent-dev python-pip liblzma-dev
+
+apt-get install gunicorn pyyaml flask flask-cors rsa
+
+sudo pip install docker-registry
+
+ 
