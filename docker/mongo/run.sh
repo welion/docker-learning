ce3060775a3afb651225f92eefc8da0f59ce9e56
welion <353566165@qq.com>
2016-05-07 00:09:24 +0800

    change mongo dockfile

--- docker/mongo/run.sh
+++ docker/mongo/run.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-if [ ! -f /.mongodb_password_set ]; then
-	/set_mongodb_password.sh
-fi
-
-if [ "$AUTH" == "yes" ]; then
-    export mongodb='/usr/bin/mongod --nojournal --auth --httpinterface --rest'
-else
-    export mongodb='/usr/bin/mongod --nojournal --httpinterface --rest'
-fi
 
 if [ ! -f /data/db/mongod.lock ]; then
-    eval $mongodb
+   /usr/bin/mongod --config /etc/mongodb.conf
 else
-    export mongodb=$mongodb' --dbpath /data/db' 
+    export mongodb=$mongodb' --dbpath /data/db'
     rm /data/db/mongod.lock
-    mongod --dbpath /data/db --repair && eval $mongodb
+    mongod --dbpath /data/db --repair && /usr/bin/mongod --config /etc/mongodb.conf
 fi
